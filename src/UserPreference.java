d4145c3c1103500519e087a7934d9865805962b5
Caleb-kim19 <caleb_kim19@handong.ac.kr>
2024-06-13 22:14:59 +0900

    Update class

--- src/UserPreference.java
+++ src/UserPreference.java
@@ -4,5 +4,42 @@ public class UserPreference {
     private double preferredPrice;
     private double preferredRating;
 
-    // Constructor, getters, and setters
+    public UserPreference(int userId, String preferredCategory, double preferredPrice, double preferredRating) {
+        this.userId = userId;
+        this.preferredCategory = preferredCategory;
+        this.preferredPrice = preferredPrice;
+        this.preferredRating = preferredRating;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getPreferredCategory() {
+        return preferredCategory;
+    }
+
+    public void setPreferredCategory(String preferredCategory) {
+        this.preferredCategory = preferredCategory;
+    }
+
+    public double getPreferredPrice() {
+        return preferredPrice;
+    }
+
+    public void setPreferredPrice(double preferredPrice) {
+        this.preferredPrice = preferredPrice;
+    }
+
+    public double getPreferredRating() {
+        return preferredRating;
+    }
+
+    public void setPreferredRating(double preferredRating) {
+        this.preferredRating = preferredRating;
+    }
 }
